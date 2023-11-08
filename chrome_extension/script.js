7dcc953a48dd5eb6171e81bfdf5707c04fc78b19
Emeteil <94739287+Emeteil@users.noreply.github.com>
2023-11-08 22:16:19 +0600

    📝 docs(README.md): add README file for the Problem button on sdamgia.ru extension 🔨 refactor(script.js): refactor script.js to improve functionality and add support for displaying answers

--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -1,20 +1,31 @@
-(function() {
+(function () {
     'use strict';
 
     var probViews = document.querySelectorAll('div.prob_view');
 
-    probViews.forEach(function(probView) {
+    probViews.forEach(async function (probView) {
         var commentsId = probView.querySelector('div[id^="comments"]');
         var decisionLink = probView.querySelector('a[href^="/problem?id="]');
-        if (commentsId && !decisionLink) {
+        if (commentsId) {
             var id = commentsId.id.replace("comments", "");
             var answerLink = document.createElement('a');
             answerLink.href = window.location.href.split(".")[0] + '.sdamgia.ru/problem?id=' + id;
             answerLink.style = "margin-left: 4px;";
             answerLink.innerText = id;
-            var probNums = probView.querySelector('span.prob_nums');
-            probNums.innerText = probNums.innerText + " №";
-            probNums.parentNode.insertBefore(answerLink, probNums.nextSibling);
+            if (!decisionLink) {
+                var probNums = probView.querySelector('span.prob_nums');
+                probNums.innerText = probNums.innerText + " №";
+                decisionLink = probNums.parentNode.insertBefore(answerLink, probNums.nextSibling);
+            }
+            try {
+                var response = await fetch(answerLink.href);
+                var data = await response.text();
+                var answer = data.match(/Ответ:?<\/span>.*\./)[0].replace("<\/span>", "").split("<\/")[0].split("Ответ")[1];
+                probView.insertAdjacentHTML("beforeEnd", `<details style="font-weight: normal; margin-left: 4px;"> <summary>Ответ</summary> <div style="position: absolute; transform: translate(47px, -19.2px);">${answer}</div> </details>`);
+                probView.style = "margin-bottom: 15px;";
+            } catch (e) {
+                console.error(e);
+            }
         }
     });
 })();
